67cb53fa75b098733825c60d48a137882aa2240d
odhisbrian43 <odhisbrian43@gmail.com>
2024-10-20 10:01:19 -0700

    Database SQL

--- task_5.sql
+++ task_5.sql
@@ -1,1 +1,1 @@
-INSERT INTO Customer (customer_id, customer_name, email, address) VALUES (1, "Cole Baidoo", "cbaidoo@sandtech,com", "123 Hapiness Ave.");
+INSERT INTO customer (customer_id, customer_name, email, address) VALUES (1, "Cole Baidoo", "cbaidoo@sandtech,com", "123 Hapiness Ave.");
