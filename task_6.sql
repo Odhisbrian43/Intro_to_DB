73e16ef10fd429aec8554627768beda4ab58474a
odhisbrian43 <odhisbrian43@gmail.com>
2024-10-20 10:05:00 -0700

    Database SQL

--- task_6.sql
+++ task_6.sql
@@ -1,2 +1,2 @@
 
-INSERT INTO customer (customer_id, customer_name, email, address) VALUES (2, "Blessing Malik", "bmalik@sandtech,com", "123 Hapiness Ave."),(3, "Obed Ehoneam", "eobed@sandtech,com", "123 Hapiness Ave."),(4, "Nehemial Kamolu", "nkamolu@sandtech,com", "123 Hapiness Ave.");
+INSERT INTO customer (customer_id, customer_name, email, address) VALUES (2, "Blessing Malik", "bmalik@sandtech.com", "123 Hapiness Ave."),(3, "Obed Ehoneam", "eobed@sandtech.com", "123 Hapiness Ave."),(4, "Nehemial Kamolu", "nkamolu@sandtech.com", "123 Hapiness Ave.");
