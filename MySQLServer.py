ed7d2fa8b3159c0e84cc28bc6975d3a732d363ee
odhisbrian43 <odhisbrian43@gmail.com>
2024-10-20 09:25:54 -0700

    Database SQL

--- MySQLServer.py
+++ MySQLServer.py
@@ -4,13 +4,13 @@ from getpass import getpass
 import mysql.connector
 from mysql.connector import connect, error
 
-CREATE DATABASE IF NOT EXISTS Intro_to_DB
+CREATE DATABASE IF NOT EXISTS alx_book_store
 try:
     with mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     password="12345",
-    database="Intro_to_BD") as connection:
+    database="alx_book_store") as connection:
         print("Database 'alx_book_store' created successfully!")
 except Error as e:
     print(e)
