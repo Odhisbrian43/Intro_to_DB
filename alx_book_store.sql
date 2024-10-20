de5575e5c213c0cc500b6c2b4e80367e07ca92bf
odhisbrian43 <odhisbrian43@gmail.com>
2024-10-20 08:30:11 -0700

    Database SQL

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -9,4 +9,4 @@ CREATE TABLE Customers ( customer_id INT PRIMARY KEY, customer_name VARCHAR(215)
 
 CREATE TABLE Orders ( order_id INT PRIMARY KEY, FOREIGN KEY (customer_id) REFERENCES Customers(customer_id), order_date DATE)
 
-CREATE TABLE Order_Details ( order_detail_id INT PRIMARY KEY, FOREIGN KEY (order_id) REFERENCES Orders(order_id), book_id REFERENCES Books(book_id), quantity DOUBLE NOT NULL)
+CREATE TABLE Order_Details ( order_detail_id INT PRIMARY KEY, FOREIGN KEY (order_id) REFERENCES Orders(order_id), FOREIGN KEY (book_id) REFERENCES Books(book_id), quantity DOUBLE NOT NULL)
